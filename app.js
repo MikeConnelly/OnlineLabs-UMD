e39d2a30ef74ac35ea4d119a02f4d7db0bfc44b5
Michael Connelly <mrc33@atlanticbb.net>
2020-07-01 00:25:05 -0400

    add sockets and get color api

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express');
+var app = express();
 var exphbs = require('express-handlebars');
 var axios = require('axios');
 var bodyParser = require('body-parser');
 var dotenv = require('dotenv');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 dotenv.config();
 const sr = process.env.SR;
@@ -11,18 +14,17 @@ const se = process.env.SE;
 const skn = process.env.SKN;
 const hub = process.env.HUB;
 const device = process.env.DEVICE;
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 const iotHubURL = `https://${hub}.azure-devices.net/twins/${device}/methods?api-version=2018-06-30`;
 
 const iotHubConfig = {
   'headers': {
     'Content-Type': 'Application/json',
-    'Authorization': `SharedAccessSignature sr=${sr}&sig=${sig}&se=${se}&skn=${skn}`  
+    'Authorization': `SharedAccessSignature sr=${sr}&sig=${sig}&se=${se}&skn=${skn}`
   }
 }
 
-var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
@@ -30,7 +32,20 @@ app.set('view engine', 'handlebars');
 app.use('/public', express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('home');
+  const data = {
+    "methodName": "getColor",
+    "responseTimeoutInSeconds": 60,
+    "payload": {}
+  };
+  axios.post(iotHubURL, data, iotHubConfig)
+    .catch(err => console.log(err))
+    .then(colorData => {
+      const data = colorData.data.replace('\u0000', '');
+      const color = JSON.parse(data).payload;
+      res.render('home', {
+        color: color
+      });
+    })
 });
 
 app.post('/api/submit', (req, res) => {
@@ -53,8 +68,24 @@ app.post('/api/submit', (req, res) => {
     "payload": {}
   };
   axios.post(iotHubURL, data, iotHubConfig)
-    .then(response => res.status(200).send('color updated'))
-    .catch(err => console.log(err));
+    .catch(err => console.log(err))
+    .then(response => res.render('home', {
+      color: color
+    }))
 });
 
-app.listen(PORT);
+http.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
+
+io.on('connection', socket => {
+  console.log('new connection');
+
+  socket.on('color update', update => {
+    io.emit('color update', update);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
