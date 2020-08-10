3e110886ad51c888d40bc40c7e8551bbb37c0b8f
Michael Connelly <mrc33@atlanticbb.net>
2020-08-09 21:01:25 -0400

    fix idle timeout time

--- utils/UserManager.js
+++ utils/UserManager.js
@@ -110,7 +110,7 @@ class UserManager {
    */
   currentUserTimoutCheck() {
     this.minutesIdle++;
-    if (this.minutesIdle >= 100) {
+    if (this.minutesIdle >= 4) {
       this.currentUserInactiveInterval = clearInterval(this.currentUserInactiveInterval);
       this.controller.resetMotorsAndClear();
       this.refreshCurrentUserTimer();
