5ebadae8e1b1d20006952b2c019eff4210017898
Michael Connelly <mrc33@atlanticbb.net>
2020-08-09 20:58:08 -0400

    update to iframe

--- app/panels/Vnc.js
+++ app/panels/Vnc.js
@@ -4,11 +4,12 @@ import VncDisplay from 'react-vnc-display';
 class Vnc extends Component {
   render() {
     return (
-      <div style={{width: 1024, height: 768 }}>
-        <VncDisplay
+      <div>
+        {/*<VncDisplay
           url="ws://192.168.1.24:8080"
           wsProtocols={['udp', 'tcp']}
-        />
+        />*/}
+        <iframe src="http://69.204.209.84:6080/" height={768} width={1024} ></iframe>
       </div>
     );
   }
