1911537c983205781e2acf5d9c9c20110add884d
Michael Connelly <mrc33@atlanticbb.net>
2020-08-21 23:44:35 -0400

    skip vnc password check

--- app/panels/Vnc.js
+++ app/panels/Vnc.js
@@ -4,7 +4,7 @@ class Vnc extends Component {
   render() {
     return (
       <div className="vnc-wrapper">
-        <iframe src="http://129.2.94.100:6080/" width={1024} height={768}></iframe>
+        <iframe src="http://129.2.94.100:6080/?password=enee205c" width={1024} height={768}></iframe>
       </div>
     );
   }
