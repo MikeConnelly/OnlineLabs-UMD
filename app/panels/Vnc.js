d3b95b615555d3edf08bbab2ce205388b2c4c311
Michael Connelly <mrc33@atlanticbb.net>
2020-09-08 19:42:22 -0400

    test new stream

--- app/panels/Vnc.js
+++ app/panels/Vnc.js
@@ -6,8 +6,8 @@ class Vnc extends Component {
     return (
       <div className="vnc-wrapper">
         {window.location.protocol === 'https:' ? <p className="proto-warn">NOTE: for video streams to work you must switch from https to http in your address bar</p> : <></>}
-        <iframe src="http://129.2.94.100:6075/?password=enee205c" width={1024} height={768}></iframe>
-        <VideoContainer url="http://192.2.94.6074/stream" />
+        <iframe src="http://129.2.94.100:6075" width={1024} height={768}></iframe>
+        <VideoContainer url="http://192.2.94.100:6074/stream" />
       </div>
     );
   }
